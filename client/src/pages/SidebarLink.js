78bce5fe6eabe19ac2a694808277defe582408ea
Jtdsiriwardena <106644708+Jtdsiriwardena@users.noreply.github.com>
2024-11-07 12:17:39 +0530

    updated

--- client/src/pages/SidebarLink.js
+++ client/src/pages/SidebarLink.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';  // Import PropTypes
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -14,8 +15,11 @@ const SidebarLink = ({ to, icon, label }) => {
     );
 };
 
-
-
-
+// Add prop types validation
+SidebarLink.propTypes = {
+    to: PropTypes.string.isRequired,       // 'to' is a required string
+    icon: PropTypes.object.isRequired,     // 'icon' should be an object (FontAwesomeIcon)
+    label: PropTypes.string.isRequired,    // 'label' should be a string
+};
 
 export default SidebarLink;
